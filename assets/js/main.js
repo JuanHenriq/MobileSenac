4b0f7403077386675edbf083a6b902965776a944
JuanHenriq <juanhenrique1d2015@gmail.com>
2024-03-14 21:22:12 -0300

    feat: no footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,25 +56,6 @@
 			)
 				.appendTo($body);
 
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
 	// Carousels.
 		$('.carousel').each(function() {
 
