b1b06a13417bd87460dfb295b7664d83b7c81584
JuanHenriq <juanhenrique1d2015@gmail.com>
2024-03-19 22:01:47 -0300

    feat: increase performance

---
+++ assets/js/feiralivre.js
@@ -0,0 +1,71 @@
+let map, infoWindow;
+
+function initMap() {
+  if (map) return;
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 16,
+  }); 
+  infoWindow = new google.maps.InfoWindow();
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        const currentposition = new google.maps.Marker({
+          position: pos,
+          map: map,
+          icon: {
+            url: "person.svg",
+          },
+        });
+
+        infoWindow.setPosition(pos);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  fetch("https://juanhenriq.github.io/api-mobile-map/feira_livre.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        const marker = new google.maps.Marker({
+          position: { lat: item.Latitude, lng: item.Longitude },
+          map: map,
+          title: item.Nome,
+        });
+
+        marker.addListener("click", () => {
+          const content = `
+            <div>
+              <h3>${item.Nome}</h3>
+              <p>Localização: ${item.Localização}</p>
+              <p>Dias: ${item.Dias}</p>
+              <p>Horário: ${item.Horário || "N/A"}</p>
+            </div>
+          `;
+
+          infoWindow.setContent(content);
+          infoWindow.open(map, marker);
+        });
+      });
+    })
+    .catch((error) => console.error("Error loading JSON file:", error));
+}
+
+window.initMap = initMap;
+
+window.addEventListener('pageshow', function(event) {
+  initMap();
+});
